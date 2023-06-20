977cec288ce707ac368ac0be0f56d4f7d6072d9c
Abernabe <toni-bernabe@hotmail.com>
2023-06-20 13:57:15 +0200

    Añadida numeros enteros VS

--- hola.java
+++ hola.java
@@ -4,6 +4,11 @@ public class HolaMundo {
 
         var numero = 2 ;
         System.out.println(numero);
+
+        int a = 3, b = 2;
+        
+        int  c = a + 5 - b;
+        System.out.println("c =" + c);
         
     }
 }
